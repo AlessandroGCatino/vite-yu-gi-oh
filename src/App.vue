02e7ad37743e5bf238d4ea11fc8d2f4319321910
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-09 17:12:24 +0100

    concluso header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
 
+    <HeaderYugi/>
+
 </template>
 
 <script>
     import axios from 'axios'
     import { store } from './store'
+    import HeaderYugi from "./components/HeaderYugi.vue"
     
     export default {
+        components:{
+            HeaderYugi,
+        },
         data() {
             return{
                 store,
---
+++ src/components/HeaderYugi.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="p-3 d-flex align-items-center ">
+        <figure>
+            <img src="../assets/img/Yu-Gi-Oh!.png" alt="Logo">
+        </figure>
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+
+</template>
+
+<script>
+
+    export default{
+        name: "HeaderYugi",
+    }
+</script>
+
+<style scoped lang="scss">
+
+    figure{
+        width: 100px;
+        margin: 0 15px;
+        img{
+            width: 100%;
+            display: block;
+        }
+    }
+
+</style>
