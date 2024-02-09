103d6ea5c6c37cd22c5317e2a2382436b96d4881
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-09 16:48:00 +0100

    creazione file e link con Store

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
 </template>
 
 <script>
+    import axios from 'axios'
+    import { store } from './store'
+    
+    export default {
+        data() {
+            return{
+                store,
+            }
+        }
+    }
 
 
 </script>
